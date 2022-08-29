7c2ba8a38cfcbcb325d80950ffafb9317e7f524d
Elaine Victoria <111344174+wjsnleeluda@users.noreply.github.com>
2022-08-30 01:15:23 +0800

    Create Weapon.java

---
+++ CLASS/Weapon.java
@@ -0,0 +1,14 @@
+public class Weapon {
+    // Weapon Attributes
+    String name;
+    int damage;
+    int weight;
+    String rarity;
+    String description;
+
+    // Weapon Method
+    public void weaponDescription() {
+        System.out.println("Object Name: " + name);
+        System.out.println("Weapon Description: " + description);
+    }
+}
